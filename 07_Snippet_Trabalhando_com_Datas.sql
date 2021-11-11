870a21d79850ee2ad7bbfb48d2a77fbc605d0167
RodrigoACM <71786041+RodrigoACM@users.noreply.github.com>
2021-11-11 17:15:34 -0300

    Update 07_Snippet_Trabalhando_com_Datas.sql

--- 07_Snippet_Trabalhando_com_Datas.sql
+++ 07_Snippet_Trabalhando_com_Datas.sql
@@ -3,7 +3,8 @@ SELECT
 	GETDATE()         AS 'TimeStamp'
 	,CONVERT(DATE, GETDATE()) as 'Data'
 	,CONVERT(DATE, DATEADD(month, DATEDIFF(m, 0, GETDATE()), 0)) AS 'PriDiaMes'
-	,CONVERT(DATE, DATEADD(s,-1,DATEADD(mm, DATEDIFF(m,0,GETDATE())+1,0))) AS 'UltDiaMes' --Se o SQL Serve for > 2012 você pode usar EOMONTH()
+	,CONVERT(DATE, DATEADD(s,-1,DATEADD(mm, DATEDIFF(m,0,GETDATE())+1,0))) AS 'UltDiaMes' --Se o SQL Serve for > 2012 vocÃª pode usar EOMONTH()
 	,(YEAR(GETDATE()) * 100) + MONTH(GETDATE()) AS 'AnoMes'
-	
+	,Convert(varchar, convert(datetime, '2021-01-11', 103), 103) as 'ConvertDeYYYMMDD'
+	,CONVERT(char(10), GetDate(),126) as 'ConvertDeYYYMMDD_OMesmo'
 	
